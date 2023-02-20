e5bbd2ee87ed9e16bee394cb3d03c5e1cd184404
fanfat <fanfat@fanfat.ru>
2023-02-20 12:27:25 +0300

    Правки для работоспособности проекта
    Осталось доделать CORS

--- Lesson_3/config/settings.py
+++ Lesson_3/config/settings.py
@@ -94,23 +94,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'db',
-#        'PASSWORD': '123',
-#        'USER': 'nikolay',
-#        'HOST': 'db', #Имя контейнера
-#        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'PASSWORD': '123',
+        'USER': 'nikolay',
+        'HOST': 'db', #Имя контейнера
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
